4dee3fc7d71e2f8ce05d77e005c65e8353088223
MoongMoongBook <j.j.kant1948@gmail.com>
2023-08-15 22:50:49 +0900

    feat : tabBar, tabController, DefaultTabController, Theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        //fontFamily: 'Roboto',
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -44,245 +29,67 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      // onDrawerChanged: (isOpened) => print(isOpened),
-      // bottomSheet: Container(
-      //   height: 100,
-      //   color: Colors.deepPurple,
-      // ),
-      // backgroundColor: Colors.yellow,
-      // resizeToAvoidBottomInset: false,
-      // extendBody: false,
-      // extendBodyBehindAppBar: true,
-      // drawerScrimColor: Colors.blueAccent,
-      // drawerEnableOpenDragGesture: true,
-      // drawerEdgeDragWidth: 10,
-      // // restorationId: ,
-      // persistentFooterAlignment: AlignmentDirectional.bottomCenter,
-      // persistentFooterButtons: [
-      //   ElevatedButton(
-      //     onPressed: () {},
-      //     child: const Text('Button'),
-      //   ),
-      //   ElevatedButton(
-      //     onPressed: () {},
-      //     child: const Text('Button'),
-      //   ),
-      // ],
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.business),
-      //       label: 'Business',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.school),
-      //       label: 'School',
-      //     ),
-      //   ],
-      // ),
-      // appBar: AppBar(
-      //   // TRY THIS: Try changing the color here to a specific color (to
-      //   // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-      //   // change color while the other colors stay the same.
-      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       appBar: AppBar(
-        leading: const Icon(Icons.abc),
-        title: const Text('AppBar'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.access_time_filled),
+          leading: const Icon(Icons.abc),
+          title: const Text(
+            'AppBar',
+            style: TextStyle(fontFamily: 'italic'),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.access_time_filled),
+            ),
+          ],
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const [
+              Icon(Icons.abc),
+              Icon(Icons.abc_outlined),
+              Icon(Icons.abc_rounded),
+            ],
+          )),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          Text(
+            'Tab Controller 1',
+            style: TextStyle(fontSize: 100, color: Colors.amber),
+          ),
+          Text(
+            'Tab Controller 2',
+            style: TextStyle(fontSize: 100, color: Colors.red),
+          ),
+          Text(
+            'Tab Controller 3',
+            style: TextStyle(fontSize: 100, color: Colors.lightBlue),
           ),
         ],
-        flexibleSpace: const Text(
-          'FLEXIBLE SPACE',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.blue,
-          ),
-        ),
-        primary: false,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(100),
-          child: Container(
-            color: Colors.red,
-            height: 100,
-          ),
-        ),
       ),
-
-      // appBar: PreferredSize(
-      //   preferredSize: MediaQuery.of(context).size * 0.3,
-      //   child: AppBar(
-      //     backgroundColor: Colors.deepPurple,
-      //     title: const Text('AppBar'),
-      //   ),
-      // ),
-      body: const Text('data', style: TextStyle(fontSize: 800)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      //
-      // drawer: Drawer(
-      //   width: 100,
-      //   child: ListView(
-      //     children: [
-      //       const DrawerHeader(
-      //         decoration: BoxDecoration(
-      //           color: Colors.deepPurple,
-      //         ),
-      //         child: Text('Drawer Header'),
-      //       ),
-      //       ListTile(
-      //         title: const Text('Item 1'),
-      //         onTap: () {
-      //           // TRY THIS: Try changing the color here to a specific color (to
-      //           // Colors.amber, perhaps?) and trigger a hot reload to see the
-      //           // ListTile change color while the other colors stay the same.
-      //           Navigator.pop(context);
-      //         },
-      //       ),
-      //       ListTile(
-      //         title: const Text('Item 2'),
-      //         onTap: () {
-      //           // TRY THIS: Try changing the color here to a specific color (to
-      //           // Colors.amber, perhaps?) and trigger a hot reload to see the
-      //           // ListTile change color while the other colors stay the same.
-      //           Navigator.pop(context);
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // endDrawer: Drawer(
-      //   width: 100,
-      //   child: ListView(
-      //     children: [
-      //       const DrawerHeader(
-      //         decoration: BoxDecoration(
-      //           color: Colors.deepPurple,
-      //         ),
-      //         child: Text('Drawer Header'),
-      //       ),
-      //       ListTile(
-      //         title: const Text('Item 1'),
-      //         onTap: () {
-      //           // TRY THIS: Try changing the color here to a specific color (to
-      //           // Colors.amber, perhaps?) and trigger a hot reload to see the
-      //           // ListTile change color while the other colors stay the same.
-      //           Navigator.pop(context);
-      //         },
-      //       ),
-      //       ListTile(
-      //         title: const Text('Item 2'),
-      //         onTap: () {
-      //           // TRY THIS: Try changing the color here to a specific color (to
-      //           // Colors.amber, perhaps?) and trigger a hot reload to see the
-      //           // ListTile change color while the other colors stay the same.
-      //           Navigator.pop(context);
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
-
-      // body: Center(
-      //   // Center is a layout widget. It takes a single child and positions it
-      //   // in the middle of the parent.
-      //   child: Column(
-      //     // Column is also a layout widget. It takes a list of children and
-      //     // arranges them vertically. By default, it sizes itself to fit its
-      //     // children horizontally, and tries to be as tall as its parent.
-      //     //
-      //     // Column has various properties to control how it sizes itself and
-      //     // how it positions its children. Here we use mainAxisAlignment to
-      //     // center the children vertically; the main axis here is the vertical
-      //     // axis because Columns are vertical (the cross axis would be
-      //     // horizontal).
-      //     //
-      //     // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-      //     // action in the IDE, or press "p" in the console), to see the
-      //     // wireframe for each widget.
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headlineMedium,
-      //       ),
-      //       Stack(
-      //         children: [
-      //           Positioned(
-      //             child: Container(
-      //               width: 100,
-      //               height: 100,
-      //               color: Colors.deepPurple,
-      //             ),
-      //           ),
-      //           Positioned(
-      //             top: 30,
-      //             child: Container(
-      //               width: 10,
-      //               height: 10,
-      //               color: Colors.red,
-      //             ),
-      //           ),
-      //         ],
-      //       )
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
